79c77a7e786e7376f91c0746bbedb60ec025cc21
abhinashwere1994 <abhinashwere1994@gmail.com>
2024-10-27 11:44:34 +0530

    Initial commit

---
+++ src/www/com/St.java
@@ -0,0 +1,13 @@
+package www.com;
+
+public class St {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("welcome file");
+		System.out.println("this file here");
+		System.out.println("hi");
+
+	}
+
+}
